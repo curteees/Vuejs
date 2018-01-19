a08595a6f511924ae22eea00d502f7e9af93f864
Curtis Lee <Curtis@eduroam-169-233-211-125.ucsc.edu>
2018-01-19 00:18:55 -0800

    first commit

---
+++ employeemanager/src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+    <nav>
+        <div class = "nav-wrapper cyan">
+            <div class = "container">
+                <router-link to = "/" class = "brand-logo">Employee Manager</router-link>
+            </div>
+        </div>
+    </nav>
+</template>
---
+++ employeemanager/src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default
+{
+    apiKey: "AIzaSyCFpWSHKOi1mIxHGYDtrrUC8-nvDN5vaVY",
+    authDomain: "vuefs-prod-9278d.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-9278d.firebaseio.com",
+    projectId: "vuefs-prod-9278d",
+    storageBucket: "vuefs-prod-9278d.appspot.com",
+    messagingSenderId: "907541436087"
+}
